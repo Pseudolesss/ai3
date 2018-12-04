6b19bc70c7096f3a03fdd10bee0174064d14396e
L'hoest Julien <julienlhoest@hotmail.com>
2018-12-04 09:40:00 +0100

    first implementation try

--- beliefstateagent.py
+++ beliefstateagent.py
@@ -42,13 +42,74 @@ class BeliefStateAgent(Agent):
 
         N.B. : [0,0] is the bottom left corner of the maze
         """
-
-        beliefStates = self.beliefGhostStates
         # XXX: Your code here
-        pass
+        width = self.walls.width
+        height = self.walls.height
+        w = 1
+        p = 0.5
+        pastBeliefStates = self.beliefGhostStates
+
+
+        beliefStates = list()
+        for i in range(len(evidences)):
+            prob = np.zeros((width, height))
+            pastProb = pastBeliefStates[i]
+            evidence = evidences[i]
+
+            k = 0
+            for x in range(evidence[0] - w, evidence[0] + w):
+                for y in range(evidence[1] - w, evidence[1] + w):
+                    if evidence[0]//width == x//width and evidence[1]//height == y//height:
+                        prob[x][y] = 1
+                        k += 1
+
+            for x in range(width):
+                for y in range(height):
+                    if prob[x][y] != 0:
+                        prob[x][y] *= self.forwarding(x, y, p, pastProb)
+
+            alpha = 1/np.sum(prob)
+            # Normalization of the probability of the evidence
+            for x in range(width):
+                for y in range(height):
+                    if prob[x][y] != 0:
+                        prob[x][y] /= alpha
+            print(np.sum(prob))
+            beliefStates.append(prob)
+
         # XXX: End of your code
         self.beliefGhostStates = beliefStates
         return beliefStates
+
+    def forwarding(self, x, y, p, pastProb):
+
+        (width, height) = pastProb.shape
+
+        pEast = 0
+        nbAdj = 0  # Nb of valid adjacent tiles
+
+        if (x-1)//width == x//width:
+            nbAdj += 1
+            pEast = p 
+        if (x+1)//width == x//width:
+            nbAdj += 1
+        if (y-1)//height == y//height:
+            nbAdj +=1
+        if (y+1)//height == y//height:
+            nbAdj +=1
+
+        summation = 0
+
+        if (x-1)//width == x//width:
+            summation += pastProb[x-1][y] * ( pEast + (1-pEast)/nbAdj )
+        if (x+1)//width == x//width:
+            summation += pastProb[x+1][y] * (1-pEast)/nbAdj
+        if (y-1)//height == y//height:
+            summation += pastProb[x][y-1] * (1-pEast)/nbAdj
+        if (y+1)//height == y//height:
+            summation += pastProb[x][y+1] * (1-pEast)/nbAdj
+
+        return summation
 
     def _computeNoisyPositions(self, state):
         """
