08ef93e30d012f32b68f57d24ebf89798d20ef48
Pseudolesss <julienlhoest@hotmail.com>
2018-12-09 16:50:52 +0100

    first try

--- beliefstateagent.py
+++ beliefstateagent.py
@@ -1,8 +1,9 @@
 # Complete this class for all parts of the project
 
 from pacman_module.game import Agent
-from pacman_module import util
+from pacman_module.pacman import Directions, GhostRules
 import numpy as np
+from pacman_module import util
 
 
 class BeliefStateAgent(Agent):
@@ -19,33 +20,38 @@ class BeliefStateAgent(Agent):
         """
         # Current list of belief states over ghost positions
         self.beliefGhostStates = None
-        # Grid of walls (assigned with 'state.getWalls()' method) 
+        # Grid of walls (assigned with 'state.getWalls()' method)
         self.walls = None
+        # Uniform distribution size parameter 'w'
+        # for sensor noise (see instructions)
+        self.w = self.args.w
+        # Probability for 'leftturn' ghost to take 'EAST' action
+        # when 'EAST' is legal (see instructions)
+        self.p = self.args.p
 
     def updateAndGetBeliefStates(self, evidences):
         """
         Given a list of (noised) distances from pacman to ghosts,
         returns a list of belief states about ghosts positions
-
         Arguments:
         ----------
         - `evidences`: list of (noised) ghost positions at state x_{t}
           where 't' is the current time step
-
         Return:
         -------
         - A list of Z belief states at state x_{t} about ghost positions
           as N*M numpy matrices of probabilities
           where N and M are respectively width and height
           of the maze layout and Z is the number of ghosts.
-
         N.B. : [0,0] is the bottom left corner of the maze
         """
+
+        beliefStates = self.beliefGhostStates
         # XXX: Your code here
         width = self.walls.width
         height = self.walls.height
-        w = 1
-        p = 0.5
+        w = self.w
+        p = self.p
         pastBeliefStates = self.beliefGhostStates
 
 
@@ -54,13 +60,10 @@ class BeliefStateAgent(Agent):
             prob = np.zeros((width, height))
             pastProb = pastBeliefStates[i]
             evidence = evidences[i]
-
-            k = 0
-            for x in range(evidence[0] - w, evidence[0] + w):
-                for y in range(evidence[1] - w, evidence[1] + w):
+            for x in range(evidence[0] - w, evidence[0] + w + 1):
+                for y in range(evidence[1] - w, evidence[1] + w + 1):
                     if x in range(width) and y in range(height):
-                        prob[x][y] = 1
-                        k += 1
+                            prob[x][y] = 1
 
             for x in range(width):
                 for y in range(height):
@@ -72,8 +75,7 @@ class BeliefStateAgent(Agent):
             for x in range(width):
                 for y in range(height):
                     if prob[x][y] != 0:
-                        prob[x][y] /= alpha
-            print(np.sum(prob))
+                        prob[x][y] *= alpha
             beliefStates.append(prob)
 
         # XXX: End of your code
@@ -83,32 +85,38 @@ class BeliefStateAgent(Agent):
     def forwarding(self, x, y, p, pastProb):
 
         (width, height) = pastProb.shape
+        sum = 0
 
-        pEast = 0
-        nbAdj = 0  # Nb of valid adjacent tiles
+        for i in (x - 1, x + 1):
+            if i in range(width):
+                sum += self.computeTerm(i, y, x, y, p, pastProb)
 
-        if (x-1) in range(width):
+        for j in (y - 1, y + 1):
+            if j in range(height):
+                sum += self.computeTerm(x, j, x, y, p, pastProb)
+
+        return sum
+
+    def computeTerm(self, i, j, x, y, p, pastProb):
+
+        nbAdj = 0
+        (width, height) = pastProb.shape
+
+        if (i+1) in range(width):
             nbAdj += 1
-            pEast = p 
-        if (x+1) in range(width):
+        if (i-1) in range(width):
             nbAdj += 1
-        if (y-1) in range(height):
+        if (j-1) in range(height):
             nbAdj += 1
-        if (y+1) in range(height):
+        if (j+1) in range(height):
             nbAdj += 1
 
-        summation = 0
+        if x == i+1 and y == j:
+            prob = (p + (1-p)/nbAdj) * pastProb[i][j]
+        else:
+            prob = (1-p)/nbAdj * pastProb[i][j]
 
-        if (x-1) in range(width):
-            summation += pastProb[x-1][y] * (pEast + (1-pEast)/nbAdj)
-        if (x+1) in range(width):
-            summation += pastProb[x+1][y] * (1-pEast)/nbAdj
-        if (y-1) in range(height):
-            summation += pastProb[x][y-1] * (1-pEast)/nbAdj
-        if (y+1) in range(height):
-            summation += pastProb[x][y+1] * (1-pEast)/nbAdj
-
-        return summation
+        return prob
 
     def _computeNoisyPositions(self, state):
         """
@@ -118,27 +126,26 @@ class BeliefStateAgent(Agent):
         """
         positions = state.getGhostPositions()
         w = self.args.w
-
-        div = float(w * w)
+        w2 = 2*w+1
+        div = float(w2 * w2)
         new_positions = []
         for p in positions:
             (x, y) = p
             dist = util.Counter()
-            for i in range(x - w, x + w):
-                for j in range(y - w, y + w):
+            for i in range(x - w, x + w + 1):
+                for j in range(y - w, y + w + 1):
                     dist[(i, j)] = 1.0 / div
+            dist.normalize()
             new_positions.append(util.chooseFromDistribution(dist))
         return new_positions
 
     def get_action(self, state):
         """
         Given a pacman game state, returns a legal move.
-
         Arguments:
         ----------
         - `state`: the current game state. See FAQ and class
                    `pacman.GameState`.
-
         Return:
         -------
         - A legal move as defined in `game.Directions`.
