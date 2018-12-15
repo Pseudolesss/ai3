1d9f24a5459388de9162f8289ef2bba82c3f5020
Pseudolesss <j.lhoest@student.uliege.be>
2018-12-15 10:09:57 +0100

    Script pour generer les graphs automatiquement

--- script.sh
+++ script.sh
@@ -1,1 +1,17 @@
-python run.py --bsagentfile beliefstateagent.py --layout observer --w 1 --p 0.5 --nghosts 5
+#!/bin/bash
+
+clear
+
+w=1
+while [ $w -le 5 ]
+do
+    x=25
+    while [ $x -le 75 ]
+    do
+        python run.py --bsagentfile beliefstateagent.py --layout observer --w $w --p 0.$x --nghosts 1 &
+        sleep 15 && kill $!
+        ((x=$x+25))
+    done
+    ((w=$w+2))
+done
+
